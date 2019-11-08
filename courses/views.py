2a0e12809b9965608ae280c923d15d33597cf800
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-08 10:36:41 +0300

    Implement course detail view

--- courses/views.py
+++ courses/views.py
@@ -6,3 +6,7 @@ from .models import Course
 def all_courses(request):
     courses = Course.objects.all()
     return render(request, 'courses/all_courses.html', {'courses': courses})
+
+def view_course(request, pk):
+    course = Course.objects.get(pk=pk)
+    return render(request, 'courses/view_course.html', {'course': course})
