bac31d2503931e2fb07341c1298a33868a82c75b
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-15 17:15:48 +0300

    Implement template tags for nav

--- courses/templatetags/course_extras.py
+++ courses/templatetags/course_extras.py
@@ -3,9 +3,17 @@ from courses.models import Course
 
 register = template.Library()
 
-@register.simple_tag
+@register.simple_tag #Option 2 in the form of a decorator
 def newest_course():
     '''Gets the most recent course that was added to the Library.'''
     return Course.objects.latest('created_at')
+
+@register.inclusion_tag('courses/course_nav.html')
+def nav_courses_list():
+    ''' Returns dict of courses to display as navigation pane.'''
+    courses = Course.objects.all()
+    return {'courses': courses}
+
+#register.inclusion_tag('courses/course_nav.html')(nav_courses_list) Option 1 for template tag that renders a new template
 
 # register.simple_tag('newest_course') #Option 1
