5b0890305a9f8fb2600de19e3b3c19caa111892e
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-15 16:49:57 +0300

    Create new template tag

---
+++ courses/templatetags/course_extras.py
@@ -0,0 +1,11 @@
+from django import template
+from courses.models import Course
+
+register = template.Library()
+
+@register.simple_tag
+def newest_course():
+    '''Gets the most recent course that was added to the Library.'''
+    return Course.objects.latest('created_at')
+
+# register.simple_tag('newest_course') #Option 1
