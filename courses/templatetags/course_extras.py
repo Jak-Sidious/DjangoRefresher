615d1237225410257bea450a6ddc0f1543788128
Jak-Sidious <30991429+Jak-Sidious@users.noreply.github.com>
2019-11-18 15:29:54 +0300

    Temp inheritence (#2)
    * Implement custom css for the app, Use of filters in templates, Using Humanise, adding filters and using variables, Conditionally render text based on some variable, truncate words, display dates and urlize, Create new template tag, Implement template tags for nav, Custom filter to estimate time based on word count, Using previously designed custom filter, Handle markup to plain text

---
+++ courses/templatetags/course_extras.py
@@ -0,0 +1,35 @@
+from django import template
+from django.utils.safestring import mark_safe
+from courses.models import Course
+import markdown2
+
+register = template.Library()
+
+@register.simple_tag #Option 2 in the form of a decorator
+def newest_course():
+    '''Gets the most recent course that was added to the Library.'''
+    return Course.objects.latest('created_at')
+
+@register.inclusion_tag('courses/course_nav.html')
+def nav_courses_list():
+    ''' Returns dict of courses to display as navigation pane.'''
+    courses = Course.objects.all()
+    return {'courses': courses}
+
+# Custom filters begin here
+@register.filter('time_estimate') 
+def time_estimate(word_count):
+    '''Estimates numbe of minutes it will take to complete a step based on the 
+    passed in wordcount.'''
+    minutes = round(word_count/20)
+    return minutes
+
+@register.filter('markdown_to_html')
+def markdown_to_html(markdown_text):
+    '''Converts markdown text to html'''
+    html_body = markdown2.markdown(markdown_text)
+    return mark_safe(html_body)
+
+#register.inclusion_tag('courses/course_nav.html')(nav_courses_list) Option 1 for template tag that renders a new template
+
+# register.simple_tag('newest_course') #Option 1
