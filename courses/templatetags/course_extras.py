e66986d7278c94e71516de1f8cbb514060e49afd
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-18 14:15:14 +0300

    Handle markup to plain text

--- courses/templatetags/course_extras.py
+++ courses/templatetags/course_extras.py
@@ -1,5 +1,7 @@
 from django import template
+from django.utils.safestring import mark_safe
 from courses.models import Course
+import markdown2
 
 register = template.Library()
 
@@ -15,12 +17,18 @@ def nav_courses_list():
     return {'courses': courses}
 
 # Custom filters begin here
-@register.filter('time_estimate')
+@register.filter('time_estimate') 
 def time_estimate(word_count):
     '''Estimates numbe of minutes it will take to complete a step based on the 
     passed in wordcount.'''
     minutes = round(word_count/20)
     return minutes
+
+@register.filter('markdown_to_html')
+def markdown_to_html(markdown_text):
+    '''Converts markdown text to html'''
+    html_body = markdown2.markdown(markdown_text)
+    return mark_safe(html_body)
 
 #register.inclusion_tag('courses/course_nav.html')(nav_courses_list) Option 1 for template tag that renders a new template
 
