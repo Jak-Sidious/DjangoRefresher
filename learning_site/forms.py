e7be4491884236713a08902e0cc80a77306b5d59
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-20 11:32:25 +0300

    Implement clean_field method with accompanying validation error

--- learning_site/forms.py
+++ learning_site/forms.py
@@ -4,3 +4,12 @@ class SuggestionForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     suggestion = forms.CharField(widget=forms.Textarea)
+    honeypot = forms.CharField(required=False, 
+                                widget=forms.HiddenInput, label="Leave Empty")
+
+    def clean_honeypot(self):
+        honeypot = self.cleanrd_data['honeypot']
+        if len(honeypot):
+            raise forms.ValidationError(
+                "Honeypot should be left empty. Bad Bot1")
+        return honeypot
