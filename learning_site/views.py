f4071af5bd2a721049671cbc57d59b77b31ae408
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-20 11:17:34 +0300

    Add email functionality to settings.py, modify from to deal with if data is valid and send email accordingly, add if to cater to display of messages

--- learning_site/views.py
+++ learning_site/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from . import forms
+
 
 
 def hello_world(request):
@@ -7,4 +12,16 @@ def hello_world(request):
 
 def suggestion_view(request):
     form = forms.SuggestionForm()
+    if request.method == 'POST':
+        form = forms.SuggestionForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                'Suggestion from {}'.format(form.cleaned_data['name']),
+                form.cleaned_data['suggestion'],
+                '{name} <{email}>'.format(**form.cleaned_data),
+                ['tickle@tickle.com']
+            )
+            messages.add_message(request, messages.SUCCESS,
+                                'Thanks for your suggestion!')
+            return HttpResponseRedirect(reverse('suggestion'))
     return render(request, 'suggestion_form.html', {'form': form})
