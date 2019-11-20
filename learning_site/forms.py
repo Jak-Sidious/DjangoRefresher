961ce4f9c6b9fcf05df18f87a240aefb59a963e4
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-20 12:45:57 +0300

    Built a custom validator

--- learning_site/forms.py
+++ learning_site/forms.py
@@ -1,15 +1,20 @@
 from django import forms
+from django.core import validators
 
+def must_be_empty(value):
+    if value:
+        raise forms.ValidationError('is not empty')
 class SuggestionForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     suggestion = forms.CharField(widget=forms.Textarea)
     honeypot = forms.CharField(required=False, 
-                                widget=forms.HiddenInput, label="Leave Empty")
+                                widget=forms.HiddenInput, label="Leave Empty",
+                                validators=[must_be_empty])
 
-    def clean_honeypot(self):
-        honeypot = self.cleanrd_data['honeypot']
-        if len(honeypot):
-            raise forms.ValidationError(
-                "Honeypot should be left empty. Bad Bot1")
-        return honeypot
+    # def clean_honeypot(self):
+    #     honeypot = self.cleanrd_data['honeypot']
+    #     if len(honeypot):
+    #         raise forms.ValidationError(
+    #             "Honeypot should be left empty. Bad Bot1")
+    #     return honeypot
