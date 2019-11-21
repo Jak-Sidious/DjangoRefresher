4f1d9f405334e8fb0fc50e42c0ee2af901713a98
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-21 12:26:12 +0300

    Add an edit quiz view, handle the rendering of data for either the Edit quiz or Create new quiz, add button to render the edit quiz, add url for the editting of a quiz

--- courses/urls.py
+++ courses/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'(?P<course_pk>\d+)/t(?P<step_pk>\d+)$', views.text_detail, name="text"),
     url(r'(?P<course_pk>\d+)/q(?P<step_pk>\d+)$', views.quiz_detail, name="quiz"),
     url(r'(?P<course_pk>\d+)/create_quiz/$', views.quiz_create, name="create_quiz"),
+    url(r'(?P<course_pk>\d+)/edit_quiz/(?P<quiz_pk>\d+)$', views.quiz_edit, name="edit_quiz"),
     url(r'(?P<pk>\d+)/$', views.view_course, name='detail'),
 ]
