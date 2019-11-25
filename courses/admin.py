68d55cc9c1a4cb3fb6b430c649690883c11dc959
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-25 18:14:06 +0300

    Create custom ordering of Quiz fields

--- courses/admin.py
+++ courses/admin.py
@@ -9,9 +9,15 @@ from . import models
 # class CourseAdmin(admin.ModelAdmin):
 #     inlines = [TextInline,]
 
+class QuizAdmin(admin.ModelAdmin):
+    fields = ['course', 'title', 'description', 'order', 'total_questions']
+
+
 admin.site.register(models.Course)
 admin.site.register(models.Text)
-admin.site.register(models.Quiz)
+admin.site.register(models.Quiz, QuizAdmin)
 admin.site.register(models.MultipleChoiceQuestion)
 admin.site.register(models.TrueFalseQuestion)
 admin.site.register(models.Answer)
+
+admin.site.site_header = "Learning Site administration"
