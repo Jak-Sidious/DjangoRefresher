bc411c6b052b484dd559d21dfc9cfde487dfb8be
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-11-26 16:33:46 +0300

    enable filtering and searching

--- courses/admin.py
+++ courses/admin.py
@@ -3,19 +3,29 @@ from django.contrib import admin
 from . import models
 
 # You can also use StackedInline or
-# class TextInline(admin.StackedInline):
-#     model = Text
+class TextInline(admin.StackedInline):
+    model = models.Text
 
-# class CourseAdmin(admin.ModelAdmin):
-#     inlines = [TextInline,]
+class QuizInline(admin.StackedInline):
+    model = models.Quiz
 
-class QuizAdmin(admin.ModelAdmin):
-    fields = ['course', 'title', 'description', 'order', 'total_questions']
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [TextInline]
+
+    serach_fields = ['title', 'description'] ##search filters
+
+    list_filter = ['created_at']
+
+# class QuizAdmin(admin.ModelAdmin):
+#     inlines = [QuizInline]
+#     # fields = ['course', 'title', 'description', 'order', 'total_questions']
+
+    
 
 
-admin.site.register(models.Course)
+admin.site.register(models.Course, CourseAdmin)
 admin.site.register(models.Text)
-admin.site.register(models.Quiz, QuizAdmin)
+admin.site.register(models.Quiz)
 admin.site.register(models.MultipleChoiceQuestion)
 admin.site.register(models.TrueFalseQuestion)
 admin.site.register(models.Answer)
