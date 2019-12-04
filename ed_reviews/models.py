cc82af29c75b29d0f066bcd74f26c750aae44fd8
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-12-04 16:24:36 +0300

    Upgrade to restframework generics, add url for the Retrive, Update and delete functionality

--- ed_reviews/models.py
+++ ed_reviews/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=255)
     url = models.URLField(unique=True)
-    
+
     def __str__(self):
         return self.title
 
@@ -15,9 +15,9 @@ class Review(models.Model):
     comment = models.TextField(blank=True, default='')
     rating = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         unique_together = ['email', 'course']
-    
+
     def __str__(self):
         return '{0.rating} by {0.email} for {0.course}'.format(self)
--- ed_reviews/urls.py
+++ ed_reviews/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "reviews"
 
 urlpatterns = [
-    url(r'^$', views.ListCreateCourse.as_view(), name='course_list')
+    url(r'^$', views.ListCreateCourse.as_view(), name='course_list'),
+    url(r'(?P<pk>\d+)/$', 
+        views.RetrieveUpdateDestroyCourse.as_view(),name='course_detail'),
 ]
