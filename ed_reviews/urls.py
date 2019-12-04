b01e54b3a95a0a36456ec9d9af6aea44ca404289
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-12-04 15:47:52 +0300

    Enable the creation of a new course from the APIView

--- ed_reviews/urls.py
+++ ed_reviews/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "reviews"
 
 urlpatterns = [
-    url(r'^$', views.ListCourse.as_view(), name='course_list')
+    url(r'^$', views.ListCreateCourse.as_view(), name='course_list')
 ]
